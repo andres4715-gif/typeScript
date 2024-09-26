83a1734f854844945188289a4bbc8b6794b6edca
andresRios <andres4715@gmail.com>
2024-09-26 13:04:31 -0500

    data working fine

--- src/generic/classes/Burger.ts
+++ src/generic/classes/Burger.ts
@@ -8,8 +8,8 @@ class Burger<T> {
   }
 }
 
-const burger = new Burger('Small', 10);
-const burger2 = new Burger(1, 10);
+const burger: Burger<string> = new Burger('Small', 10);
+const burger2: Burger<number> = new Burger(1, 10);
 
 console.log(burger);
 console.log(burger2);
