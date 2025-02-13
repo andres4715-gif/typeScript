aa6baba81ff189a79f06c4b9b985da2857f4e312
Andres Rios <andres.rios@intelex.com>
2025-02-13 11:59:43 -0500

    initial commit

--- src/abstractClass/buyCar/types.ts
+++ src/abstractClass/buyCar/types.ts
@@ -1,18 +1,18 @@
 export type carData = {
-  brand: string;
-  model: string;
-  year: number;
-  type: string;
-  color: string;
-  enginePower: number;
-  maxSpeed: number;
-  price: number;
+    brand: string;
+    model: string;
+    year: number;
+    type: string;
+    color: string;
+    enginePower: number;
+    maxSpeed: number;
+    price: number;
 };
 
 export type requirement = {
-  brand: string;
-  model: string;
-  color: string;
-  mileage?: number;
-  year: number;
+    brand: string;
+    model: string;
+    color: string;
+    mileage?: number;
+    year: number;
 };
--- src/abstractClass/buyCar/vehicle.ts
+++ src/abstractClass/buyCar/vehicle.ts
@@ -1,26 +1,24 @@
 /*
-Is possible to have abstract properties and methods 
-- for abstract properties is not necessary to add this on the constructor 
+Is possible to have abstract properties and methods
+- for abstract properties is not necessary to add this on the constructor
 */
 
-abstract class Vehicle {
-  readonly brand: string;
-  readonly model: string;
-  readonly year: number;
-  readonly type: string;
+export default abstract class Vehicle {
+    readonly brand: string;
+    readonly model: string;
+    readonly year: number;
+    readonly type: string;
 
-  constructor(brand: string, model: string, year: number, type: string) {
-    this.brand = brand;
-    this.model = model;
-    this.year = year;
-    this.type = type;
-  }
+    constructor(brand: string, model: string, year: number, type: string) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.type = type;
+    }
 
-  displayCarData(): string {
-    return `${this.brand} ${this.model}`;
-  }
+    displayCarData(): string {
+        return `${this.brand} ${this.model}`;
+    }
 
-  abstract carAvailability(brand: string): void;
+    abstract carAvailability(brand: string): void;
 }
-
-export default Vehicle;
